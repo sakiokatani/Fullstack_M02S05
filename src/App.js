4d8fded96a23c446971d0caeddf3c439c8fd52e6
saki <saki.okatani1@gmail.com>
2023-06-15 17:37:53 -0300

    hello world

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import './styles/App.css';
+import Header from './components/header'
+import Main from './components/main'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <p className='FirstPage'>Hello World!!!</p>
-      </header>
+      <Header className="App-header"/>
+      <Main/>
     </div>
   );
 }
+
 
 export default App;
